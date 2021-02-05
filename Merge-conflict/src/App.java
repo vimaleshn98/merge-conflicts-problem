766a8e15a5f38796704355e3ad69f7da3e3bd5f5
vimaleshn98 <vimaleshn98@gmail.com>
2021-02-05 08:27:58 +0530

    co

--- Merge-conflict/src/App.java
+++ Merge-conflict/src/App.java
@@ -2,10 +2,6 @@
 public class App {
 		public static void main(String[] args) {
 			//change A
-<<<<<<< HEAD
-			//change D
-=======
 			//change B
->>>>>>> branch 'main' of https://github.com/vimaleshn98/merge-conflicts-problem.git
 		}
 }
