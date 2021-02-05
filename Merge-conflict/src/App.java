8d4ecbd2b3e2c15132cc0e77d06bc13c70525445
vimaleshn98 <vimaleshn98@gmail.com>
2021-02-05 09:44:43 +0530

    tester 2 added

--- Merge-conflict/src/App.java
+++ Merge-conflict/src/App.java
@@ -6,6 +6,6 @@ public class App {
 			//change K
 			System.out.println("Developer 2");
 			System.out.println("Developer 1");
-			System.out.println("tester 1");
+			System.out.println("tester 2");
 		}
 }
